0214f03356b3f3b2f414afba5825b09ea0b8fada
wesssel <wessel.vanderpal@vergleich.org>
2018-08-11 14:49:46 +0200

    bet slips ui

--- src/components/BetSlip.vue
+++ src/components/BetSlip.vue
@@ -1,6 +1,38 @@
 <template>
-  <div class="bet-slip">
-    <pre>{{betSlip}}</pre>
+  <div class="box">
+    <ul>
+      <li>
+        Better: <strong>{{betSlip.better}}</strong>
+      </li>
+      <li>
+        Stake: <strong>ETH {{betSlip.amount}}</strong>
+      </li>
+      <li>
+        Odds: <strong>{{betSlip.odd}}</strong>
+      </li>
+      <li>
+        <br>
+        Bets:
+        <ul>
+          <li v-for="(bet, index) in betSlip.bets" :key="index">
+            <div v-if="bet.fixture">
+            <span :class="{ 'has-background-success': bet.side === 1 }">
+              {{bet.fixture.home.team}}
+            </span>
+              -
+              <span :class="{ 'has-background-success': bet.side === 2 }">
+              Draw
+            </span>
+              -
+              <span :class="{ 'has-background-success': bet.side === 3 }">
+              {{bet.fixture.away.team}}
+            </span>
+            </div>
+            <img v-else class="spinner__icon" src="~/assets/loading.svg" alt="loading">
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,3 +46,10 @@
     },
   }
 </script>
+
+<style>
+  .box {
+    margin-bottom: 20px;
+  }
+
+</style>
