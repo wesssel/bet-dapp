290057e042472b83c291b9958afdfeab69ce9a0e
wesssel <wessel.vanderpal@vergleich.org>
2018-08-24 15:56:07 +0200

    add comments

--- contracts/BetSlips.sol
+++ contracts/BetSlips.sol
@@ -2,16 +2,29 @@ pragma solidity ^0.4.24;
 
 import './lib/Model.sol';
 
+/** @title Bet slips. */
 contract BetSlips {
+    /** @dev Map of BetSlips */
     mapping(uint => Model.BetSlip) public betSlips;
+    /** @dev Total count of betSlips */
     uint public betSlipsCount;
 
+    /** @dev Adds a bet slip to the block
+      * @param _fixtureIds All fixture ids of a bet slip
+      * @param _sides All sides chosen of a bet in a bet slip
+      * @param _amount Total amount in a bet slip
+      * @param _odd Total odd of bet slip
+      */
     function addBetSlip(uint[] _fixtureIds, uint[] _sides, uint _amount, uint _odd) public {
         betSlipsCount++;
         betSlips[betSlipsCount] = Model.BetSlip(msg.sender, _amount, _odd, _fixtureIds.length);
         addBetsToSlip(_fixtureIds, _sides);
     }
 
+    /** @dev Adds bets to a bet slip
+      * @param _fixtureIds All fixture ids of a bet slip
+      * @param _sides All Sides chosen of a bet in a bet slip
+      */
     function addBetsToSlip(uint[] _fixtureIds, uint[] _sides) private {
         for (uint i = 0; i < _fixtureIds.length; i++) {
             betSlips[betSlipsCount].bets[i].fixtureId = _fixtureIds[i];
@@ -19,7 +32,15 @@ contract BetSlips {
         }
     }
 
-    function getBetSlip(uint _id) public view returns(address better, uint amount, uint odd, uint[] fixtureId, uint[] sides) {
+    /** @dev Get bets slip by id
+      * @param _id Id of bet slip
+      * @return better Better of bet slip
+      * @return amount Amount of bet slip
+      * @return odd Total odds of bet slip
+      * @return fixtureIds Fixture ids of bet slip
+      * @return sides Chosen sides of bet slip
+      */
+    function getBetSlip(uint _id) public view returns(address better, uint amount, uint odd, uint[] fixtureIds, uint[] sides) {
         uint[] memory _fixtureIds = new uint[](betSlips[_id].betsCount);
         uint[] memory _sides = new uint[](betSlips[_id].betsCount);
 
--- contracts/MainAccount.sol
+++ contracts/MainAccount.sol
@@ -1,13 +1,21 @@
 pragma solidity ^0.4.24;
 
+/** @title Main account. */
 contract MainAccount {
+    /** @dev Hardcoded main account */
     address constant public mainAccount = 0xCf3105d772A86966a4DA63C4e86dC6C47701c2d1;
 
-    function getMainAccountBalance() public view returns(uint256) {
+    /** @dev Returns balance of main account
+      * @return balance Balance of main account.
+      */
+    function getMainAccountBalance() public view returns (uint256 balance) {
         return mainAccount.balance;
     }
 
-    function getMainAccount() public view returns(address) {
+    /** @dev Returns main account
+      * @return account Address main account
+      */
+    function getMainAccount() public view returns (address account) {
         return mainAccount;
     }
 }
